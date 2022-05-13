0e7208dd082e9092cacd1e99aa9c4074b591bd64
Tanner Garlick <tannerjgarlick@gmail.com>
2022-05-12 20:38:19 -0600

    initial commit

---
+++ Intentional Relationships/Intentional_RelationshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intentional_RelationshipsApp.swift
+//  Intentional Relationships
+//
+//  Created by Tanner Garlick on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Intentional_RelationshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
